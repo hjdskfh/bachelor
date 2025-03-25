47a37cbada083624df393d1cefd2ff64c9288da1
Lea Bauer <leavic98@helvetica.kip.uni-heidelberg.de>
2025-03-25 16:57:19 +0100

    now really add data

---
+++ run_hist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p einc
+#SBATCH --job-name=sim_run
+#SBATCH --mem=256G
+#SBATCH --cpus-per-task=64
+#SBATCH --gres=gpu:1
+#SBATCH --time=4:00:00
+#SBATCH --output=output.log
+#SBATCH --error=error.log
+
+# Run Python script inside the Singularity container
+singularity exec --app dls /containers/stable/2025-02-19_1.img python ~/NeuMoQP/Programm/code/main_hist.py
+
